9b2bcbde4af6a95098cd492d6d99d4b3fc845625
Max Thomson <git@mnt.dev>
2022-11-13 21:02:48 -0800

    [Blobs] Initial Blobs

--- src/blobs.ts
+++ src/blobs.ts
@@ -1,8 +1,16 @@
-import { CFRequest } from "./index";
+import { CFRequest, Env } from "./index";
 
-async function blobs(req: CFRequest) {
-  console.log("Blob", req.path);
-  return new Response();
+async function blobs(req: CFRequest, env: Env) {
+  // DB Query
+  let data = await env.containerFlareR2.get(req.tag);
+  console.log("DATA:", !!data);
+  if (!data) {
+    return new Response("{}", { status: 404 });
+  }
+
+  let resp = new Response(await data.arrayBuffer());
+
+  return resp;
 }
 
 export default blobs;
