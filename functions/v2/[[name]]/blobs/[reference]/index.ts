10b217def29dd35454db4e72b676a870bc377b88
Max Thomson <git@mnt.dev>
2023-01-13 17:04:15 -0800

    Revert: cache

--- functions/v2/[[name]]/blobs/[reference]/index.ts
+++ functions/v2/[[name]]/blobs/[reference]/index.ts
@@ -15,48 +15,27 @@ export const onRequest: PagesFunction<Env> = async (
   const { name, reference, error } = parseParams(context.params);
   console.log({ name, reference, error });
   if (error) {
-    return new Response(
-      `{"errors": [{
-          "code": "123",
-          "message": "Bad Params",
-          "detail": "Bad Params"
-          }]}`,
-      { status: 404 }
-    );
+    return error;
   }
 
-  let cache = await caches.open("blob:cache");
-  let cacheKey = new Request(context.request.url, context.request);
-  let response = await cache.match(cacheKey);
-  console.log("CAHCE HIT:", !!response);
-  if (!response) {
-    // DB Query
-    let data = await context.env.containerFlareR2.get(reference);
-    console.log("DATA:", !!data);
-    if (!data) {
-      response = new Response(
-        `{"errors": [{
+  let response = new Response();
+
+  // DB Query
+  const data = await context.env.containerFlareR2.get(reference);
+  if (!data) {
+    response = new Response(
+      `{"errors": [{
           "code": "123",
           "message": "No Data",
           "detail": "No Data"
         }]}`,
-        {
-          status: 404,
-          headers: {
-            "Cache-Control": "s-maxage=86400",
-          },
-        }
-      );
-    } else {
-      // let resp = new Response(data.body);
-      response = new Response(data.body, {
-        headers: {
-          "Cache-Control": "s-maxage=86400",
-        },
-      });
-    }
-
-    context.waitUntil(cache.put(cacheKey, response.clone()));
+      {
+        status: 404,
+      }
+    );
+  } else {
+    response = new Response(data.body);
   }
+
   return response;
 };
