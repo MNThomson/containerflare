26319337ac437ed628e6dcc8c49d13eaf7da0f52
Max Thomson <git@mnt.dev>
2023-01-15 18:18:30 -0800

    Version endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// import preact from "@astrojs/preact";
 import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-// https://github.com/rodneylab/astro-cloudflare
 export default defineConfig({
-  // integrations: [preact()],
   experimental: {
     prerender: true,
     errorOverlay: true,
