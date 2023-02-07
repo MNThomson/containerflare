c33122a07c59a514e699e47b19e49aa839e2d24e
Max Thomson <git@mnt.dev>
2023-02-07 09:54:55 -0800

    [Deps] Upgrade to Astro v2 (#26)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    prerender: true,
-    errorOverlay: true,
-  },
   publicDir: "./src/public",
   site: `https://cfcr.dev`,
   output: "server",
