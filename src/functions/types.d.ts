1e7da446c3ea93a2fab827a1e1b6ac5bfc97d5c8
Max Thomson <git@mnt.dev>
2023-11-16 23:48:18 -0800

    Delete functions

--- src/functions/types.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Response as WorkerResponse } from "@cloudflare/workers-types";
-
-// `@cloudflare/workers-types` conflicts with `@types/node`, so we can't include
-// it in the `tsconfig.json` `types` array
-declare global {
-  const Response: typeof WorkerResponse;
-}
